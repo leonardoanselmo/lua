609f5f4c10d954072ee4e94d41171ff7e5a7c37b
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2015-10-05 20:37:37 -0300

    Arquivo modificado.

--- media_e_produto.lua
+++ media_e_produto.lua
@@ -2,14 +2,14 @@ require("luaunit")
 
 luaunit = require('luaunit')
 
-function media(n1, n2)
+function calcula_media(n1, n2)
 
 	media = (n1+n2)/2
 	return media
 
 end
 
-function produto(n1, n2)
+function calcula_produto(n1, n2)
 
 	produto = (n1*n2)
 	return produto
@@ -19,13 +19,13 @@ end
 
 function testMedia()
 
-	luaunit.assertEquals(2, media(2,2))
+	luaunit.assertEquals(2, calcula_media(2,2))
 
 end
 
 function testProduto()
 
-	luaunit.assertEquals(4, produto(2,2))
+	luaunit.assertEquals(4, calcula_produto(2,2))
 
 end
 
